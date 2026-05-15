889a07b240fcc329b6213134f177226e154c7201
hovakimyana536 <hovakimyana563@gmail.com>
2026-05-15 19:42:15 +0400

    xo game

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// React.js_ը մեր Փոխարեն Պատրաստում է Դետալններ;
+// Գլխավոր #root, 2ական > .App;
+
+import './App.css';
+import Game from './Game';
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
